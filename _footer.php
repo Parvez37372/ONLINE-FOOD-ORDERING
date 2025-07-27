f66e50bcb16154830d7023aab5231c2b03ac30fd
Parvez Ahmad <142169142+Parvez37372@users.noreply.github.com>
2025-07-27 14:07:20 +0530

    Add files via upload

---
+++ _footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Devloped By RDX  <a href="https://github.com/darshankparmar" target="_blank" rel="noopener noreferrer">Al-Umar-Resturnat</a></p>
+</div>
